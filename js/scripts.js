96c22fe8921cf64473618983973608a2d0198458
Guest User <Guest@epicodus-10.local>
2018-01-26 13:47:57 -0800

    Add input forms. Add toString method for Pizza.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
+var myPremadePizzaList = [];//Will store standard Pizzas
+var myPizzaOrders = []; //Will store all my stores current Pizza orders.
+
 function Pizza(nameIn, sizeIn, crustIn, toppingsIn){
   this.name = nameIn;
   this.size = sizeIn;
@@ -5,11 +8,26 @@ function Pizza(nameIn, sizeIn, crustIn, toppingsIn){
   this.toppings = toppingsIn;
 }
 Pizza.prototype.cost = function(){
-  var thisPizzasCost;
+  var thisPizzasCost = 0;
   thisPizzasCost += this.size/2 * 2; //Formula I'm using to calculate cost of Pizza based on Size;
-  thisPizzasCost += 2*(this.toppings.length); //Costs 2 Dollars per topping
-  if(this.crust == special){
+  thisPizzasCost += 2*(this.toppings.length); //Costs 2 Dollars per topping not including first.
+  if(this.crust == "Special"){
     thisPizzasCost += 2;
   }
   return thisPizzasCost;
 }
+Pizza.prototype.toString = function(){
+  return "Name: " + this.name + "Size: " + this.size + "Crust: " + this.crust + "Toppings: " + this.toppings.toString() + "Cost: " + this.cost();
+}
+$(document).ready(function(){
+  $("filler").click(function(){
+  });
+  $("form#pizzaForm").submit(function(event) {
+  event.preventDefault();
+
+  });
+});
+
+var one = new Pizza("Cheese", 12, "Normal", ["Anchovies, Tomatoes"]);
+one.cost();
+one.toString();
